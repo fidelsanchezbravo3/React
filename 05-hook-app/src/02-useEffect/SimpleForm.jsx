525caad8df59c727c39fbff37cdbbc648c5274a4
fidelsanchezbravo3 <adrianbra07@gmail.com>
2022-12-04 21:14:05 -0600

    Avance seccion 9

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,63 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Message } from "./Message";
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'fernando@google.com'
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { value, name } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+        });
+    }
+
+    useEffect( () => {
+        // console.log('useEffect called!');
+    }, []);
+
+    useEffect( () => {
+        // console.log('formState changed');
+    }, [formState]);
+
+    useEffect( () => {
+        // console.log('email changed');
+    }, [email]);
+
+
+
+  return (
+    <>
+        <h1>Formulario Simple</h1>
+        <hr />
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username" 
+            value={ username }
+            onChange={ onInputChange }
+        />
+
+        <input 
+            type="email"
+            className="form-control mt-2"
+            placeholder="fernando@google.com"
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+        />
+
+        {
+            (username === 'strider') && <Message />
+        }
+    </>
+  );
+}
---
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,30 @@
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "./";
+
+export const MultipleCustomHooks = () => {
+
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
+    const { quote, author } = !!data && data[0];
+
+    return (
+    <>
+        <h1>BrealingBad Quotes</h1>
+        <hr />
+        
+        {
+            (isLoading) 
+                ? <LoadingQuote />
+                : <Quote quote={ quote } author={ author } />
+        }
+
+        <button 
+            className="btn btn-primary" 
+            onClick={ () => increment() }
+            disabled={ isLoading }>
+                Next quote
+        </button>
+
+    </>
+  );
+}
