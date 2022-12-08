32883ff80c2bd3a7bb2aad27f2f1c1addb48c637
Fidel Adrian Sanchez Bravo <fasanchezb@liverpool.com.mx>
2022-12-08 11:16:36 -0600

    Fin de la seccion 11

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage } from "./AboutPage";
+import { UserProvider } from "./context/UserProvider";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { Navbar } from "./Navbar";
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
         {/* <h1>MainApp</h1> */}
         <Navbar />
         <hr />
@@ -17,6 +18,6 @@ export const MainApp = () => {
           {/* <Route path="/*" element={ <LoginPage /> } /> */}
           <Route path="/*" element={ <Navigate to="/about"/> } />
         </Routes>
-    </>
+    </UserProvider>
   );
 }
