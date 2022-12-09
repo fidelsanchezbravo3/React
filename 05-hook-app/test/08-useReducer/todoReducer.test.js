f7be001dfe80f4940314803f914cbd9f8d4217f5
Fidel Adrian Sanchez Bravo <fasanchezb@liverpool.com.mx>
2022-12-09 10:49:57 -0600

    Avance en la seccion 12

---
+++ 05-hook-app/test/08-useReducer/todoReducer.test.js
@@ -0,0 +1,5 @@
+describe('Pruebas en el todoReducer', () => { 
+    test('Debe de ', () => { 
+
+     });
+ });
--- 05-hook-app/test/hooks/useCounter.test.js
+++ 05-hook-app/test/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useCounter } from "../../src/hooks/useCounter";
 
 describe('Pruebas en el useCounter', () => { 
@@ -22,4 +22,46 @@ describe('Pruebas en el useCounter', () => {
         expect( counter ).toBe(100);
 
      });
+
+     test('Debe de incrementar el contador', () => { 
+
+        const { result } = renderHook( () => useCounter() );
+        const { increment } = result.current;
+
+        act( () => {
+            increment();
+            increment(2)
+        });
+        
+        expect( result.current.counter ).toBe(13);
+
+      });
+
+      test('Debe de decrementar el contador', () => { 
+
+        const { result } = renderHook( () => useCounter() );
+        const { decrement } = result.current;
+
+        act( () => {
+            decrement();
+            decrement(2);
+        });
+
+        expect( result.current.counter ).toBe(7);
+
+       });
+
+       test('Debe de regresar al valor inicial', () => {
+
+        const { result } = renderHook( () => useCounter() );
+        const { reset } = result.current;
+
+        act( () => {    
+            reset();
+        })
+
+        expect( result.current.counter ).toBe(10);
+
+       });
+
  });
